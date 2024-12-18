373bd781f6da991bbdb5632add89ef5fd9496049
zhangmo8 <wegi866@gmail.com>
2024-12-18 11:25:57 +0800

    chore: support cf deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://zm8.dev",
   integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
+  adapter: cloudflare(),
 });
